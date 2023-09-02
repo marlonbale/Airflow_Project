7cf5bd4bc0f1d027f93caf3c7b77b14ec4b2efcb
marlonbale <117773109+marlonbale@users.noreply.github.com>
2023-09-02 10:36:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 import requests
 import pandas as pd
+import json
+from datetime import datetime
+import s3fs
 
-api_key = "553fea112a6b411f81e204522233107"
-city = 'New_York'
-end_point = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}'
+def weather_etl():
+    api_key = "553fea112a6b41****"
+    city = 'New_York'
+    end_point = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}'
 
 
-#make the API requests
-response = requests.get(end_point)
-data = response.json()
+    #make the API requests
+    response = requests.get(end_point)
+    data = response.json()
 
-#print(data)
+    #print(data)
 
-#data we need
-location = data['location']['name']
-temperature_c = data['current']['temp_c']
+    #data we need
+    location = data['location']['name']
+    temperature_c = data['current']['temp_c']
 
-weather_list = []
+    weather_list = []
 
-weather_data = {"location": location,
-    "temperature_c": temperature_c,
-}
+    weather_data = {"location": location,
+        "temperature_c": temperature_c,
+    }
 
-print(weather_data)
+    print(weather_data)
 
-weather_list.append(weather_data)
+    weather_list.append(weather_data)
 
-#create  a pandas dataframe
+    #create  a pandas dataframe
 
-df = pd.DataFrame(weather_list)
-df.to_csv("NYC_data.csv")
+    df = pd.DataFrame(weather_list)
+    #df.to_csv("NYC_data.csv")
+    
+    #saving the csv to s3bucket
+    df.to_csv("s3://marlon-airflow-bucket/NYC_data.csv")
 
 
 
